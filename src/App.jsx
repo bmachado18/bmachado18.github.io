451d67ed9b506a9b91fb6a8144a14dae977692a2
Brent Machado <brentmachado7@gmail.com>
2024-05-06 10:28:02 -0700

    Created layout and pages; incorporated router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
+
+import MainLayout from './layouts/MainLayout'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import ProjectsPage from './pages/ProjectsPage'
+import NotFoundPage from './pages/NotFoundPage'
+
 
 const App = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">Hello World</h1>
-  )
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+          <Route path='/' element={<MainLayout />}>
+            <Route index element={<HomePage />}/>
+            <Route path='/about' element={<AboutPage />}/>
+            <Route path='/projects' element={<ProjectsPage />}/>
+            <Route path='*' element={<NotFoundPage />}/>
+          </Route>
+        )
+      )
+    
+      return <RouterProvider router={router}/>
 }
 
 export default App
