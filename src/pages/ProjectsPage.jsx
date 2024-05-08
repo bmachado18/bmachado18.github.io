67042d2eaeac14db1b3632e9dc083e1b93f08c64
Brent Machado <brentmachado7@gmail.com>
2024-05-08 11:07:39 -0700

    updated unfinished content

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,6 +1,25 @@
+import SocialButtons from '../components/SocialButtons'
+
 const ProjectsPage = () => {
   return (
-    <div>ProjectsPage</div>
+    <section className="bg-primary px-5 pt-5 py-5">
+      <div className=" md-w-[60%] mx-auto text-white">
+        <h2 className="text-4xl font-bold mb-5">
+          Thanks for stopping by!
+        </h2>
+        <p className="py-5">
+            I haven't got to this yet.. oops!
+        </p>
+        <p className="py-5">
+            I am currently looking for a 4 or 12 month coop as a Software Developer. If you
+            want to discuss coops or my projects feel free to email me or reach me at my socials anytime!
+        </p>
+        <div className="py-10">
+          <SocialButtons />
+        </div>
+      
+      </div>
+    </section>
   )
 }
 
